1116594286c62c8ab04c2972ea639a4dabc74c92
Tu Nombre <you@example.com>
2022-11-20 18:03:57 +0100

    Añadiendo el archivo de participante

---
+++ participante.h
@@ -0,0 +1,15 @@
+#ifndef _PARTICIPANTE_
+#define _PARTICIPANTE_
+
+#include <iostream>
+#include <string>
+#include "usuarioregistrado.h"
+
+class Participante:UsusarioRegistrado{
+    private:
+
+    public:
+}
+
+
+#endif
