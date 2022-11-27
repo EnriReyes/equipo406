68b44b346273473093357d4804b3e4ff901c5a07
Enrique de los Reyes <i12remoe@uco.es>
2022-11-27 20:20:44 +0100

    clase filecurso

--- prueba.cc
+++ prueba.cc
@@ -12,7 +12,6 @@ int main(){
     std::cout<<"Inserte el nombre del curso.\n";
     getline(std::cin, aux);
     Curso c(aux);
-    std::cout << c.get_size_participantes() << std::endl;
 
     std::cout<<"Inserte la descripcion del curso.\n";
     getline(std::cin, aux);
@@ -52,20 +51,6 @@ int main(){
     participante.inscribirse_curso(c);
     std::cout << c.get_size_participantes() << std::endl;
 
-        f.guardar_curso(c);
-        f.leer_curso(b);
-        std::cout<< c.get_id()<<" "<<c.get_descripcion()<<" "<<c.get_fecha_inicio()<<" "<<c.get_fecha_final()<<" "<<c.get_aforo()<<std::endl;
-        auto l = c.get_lista_participantes();
-        auto v = c.get_valoracion();
-
-        i=0;
-        for(auto list = l.begin(); list!=l.end(); list++){
-                std::cout<<*list<<" "<<v[i]<< std::endl;
-        } 
-
-        auto j = f.get_vector_cursos();
-
-        std::cout<<j[0]<<"\nHola";
 }
 
 //Comentario de prueba para gitlens
