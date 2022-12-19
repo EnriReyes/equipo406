d72263a9eb7a306e2f6b8a51e67aa23c409e8b9a
i12vimoj <javiervioquemoreno@gmail.com>
2022-12-19 14:53:06 +0100

    Añado el segundo test de filedatos

--- tests/filedatos_test.cc
+++ tests/filedatos_test.cc
@@ -8,7 +8,12 @@ TEST(Filedatos, filedatos_obtener_datos) {
   UsuarioReg aux(" ", " ", " ", " ", 0);
   File_datos fd;
   aux=fd.obtener_datos("31020644S");
-
+  EXPECT_EQ("31020644S",aux.get_dni());
+  EXPECT_EQ("Pepe Federico García",aux.get_nombrecompleto());
+  EXPECT_EQ("mail@mail.mail",aux.get_email());
+  EXPECT_EQ("login",aux.get_uco_login());
+  EXPECT_EQ(1,aux.get_id());
+  aux=fd.obtener_datos2("login");
   EXPECT_EQ("31020644S",aux.get_dni());
   EXPECT_EQ("Pepe Federico García",aux.get_nombrecompleto());
   EXPECT_EQ("mail@mail.mail",aux.get_email());
