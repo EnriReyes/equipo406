18967072f22361656de4199b5f8a7253ad6c0dbf
i12remoe <118127017+i12remoe@users.noreply.github.com>
2022-12-19 19:31:12 +0100

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/javier/Escritorio/equipo406-1
-# Build directory: /home/javier/Escritorio/equipo406-1/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("tests")
--- build/src/filelogin/CMakeFiles/filelogin.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for filelogin.
-# This may be replaced when dependencies are built.
